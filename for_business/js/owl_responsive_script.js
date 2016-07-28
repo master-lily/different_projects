ffcaec360077df0e0c108644d5f548048a9c045b
master-lily <losiklily@gmail.com>
2016-07-28 23:32:05 +0300

    made banner

---
+++ for_business/js/owl_responsive_script.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function() {
+    $(".owl__slider").owlCarousel({
+        navigationText : false,
+        responsive: true,
+        singleItem:true,
+        navigation: true,
+        pagination: true
+        //panigationNumber: 5
+    });
+
+});
